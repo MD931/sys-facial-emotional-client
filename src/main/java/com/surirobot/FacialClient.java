f3c16d4bd85c8ad58c7e442ea5edbedd24dc9806
MD931 <m.habchi12@gmail.com>
2018-01-23 17:18:51 +0100

    maj

--- src/main/java/com/surirobot/FacialClient.java
+++ src/main/java/com/surirobot/FacialClient.java
@@ -4,27 +4,26 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Panel;
 import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.Base64;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.TargetDataLine;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.protocol.HTTP;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -58,13 +57,15 @@ public class FacialClient extends JFrame {
 
 	}
 	//Url de l'api
-	public static String URL_API = "http://localhost:8080/emotions/actions/retrieve-facial-emotion";
+	public static String URL_API = "http://localhost:8080/emotions/actions/retrieve-video-emotion";
 	
 	//Label ou afficher les image et le résultat
 	public static JLabel camera;
 	public static JLabel result;
+	static final long RECORD_TIME = 10000;  // 1 minute
 	
 	private static AtomicInteger counter = new AtomicInteger(0);
+	private static AtomicInteger counterRecord = new AtomicInteger(0);
 	
 	
 	private static final class Capture extends Thread {
@@ -77,66 +78,62 @@ public class FacialClient extends JFrame {
 
 		@Override
 		public void run() {
+			
 			//Initialiser la webcam et l'ouvrir
 			Webcam webcam = Webcam.getDefault();
 			webcam.setViewSize(WebcamResolution.QVGA.getSize());
 			webcam.open();
-			
+			JSONArray ja = new JSONArray();
+			final JavaSoundRecorder recorder = new JavaSoundRecorder();
+			new Thread(()-> {
+				recorder.start();
+			}).start();
 			while (true) {
 				if (!webcam.isOpen()) {
 					break;
 				}
-				
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				//Récuperer une image depuis la webcam
 				BufferedImage image = webcam.getImage();
+				
 
 				if (image == null) {
 					break;
 				}
-				JSONObject js = new JSONObject();
-				JSONArray ja = new JSONArray();
+				
 				
 				//On encode notre image en Base64 et on la met dans un JSON
-				ja.put(encoder(image));
+				//ja.put(encoder(image));
 				
 				//On affiche notre image sur l'écran
 				ImageIcon icon = new ImageIcon(image);
 				camera.setIcon(icon);
 				
 				int n = counter.incrementAndGet();
+				int m = counterRecord.incrementAndGet();
 				
-				//Dés qu'on aura 25 images on envoi à l'API
+				//Dés qu'on aura 20 images on envoi à l'API
 				if (n != 0 && n % 25 == 0) {
 					counter.set(0);
-					
-					//On construit la requete
-					HttpClient client = new DefaultHttpClient();  
-					HttpPost post = new HttpPost(URL_API);
-					HttpResponse response;
-			        StringEntity se;
-					try {
-						se = new StringEntity(js.put("pictures", ja).toString());
-				        se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
-				        post.setEntity(se);
-				        response = client.execute(post);
-				        BufferedReader bis = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-				        StringBuilder s = new StringBuilder();
-				        String tmp = "";
-				        
-				        //On lis le résultat
-				        while((tmp = bis.readLine())!=null) {
-				        	s.append(tmp);
-				        }
-				        System.out.println("EMOTION ==== "+s.toString());
-				        
-				        //On affiche le résultat à l'écran
-				        result.setText(new JSONObject(s.toString()).optString("emotion", "null"));
-				        
-					} catch (UnsupportedEncodingException e) {
-						e.printStackTrace();
-					} catch (IOException e) {
-						e.printStackTrace();
+					ja.put(encoder(image));
+					if(m!= 0 && m % 250 == 0) {
+						counterRecord.set(0);
+						recorder.finish();
+						new Worker(ja, new JSONObject().put("record", encoder("./RecordAudio.wav"))).start();
+						recorder.deleteFile();
+						new Thread(()-> {
+							recorder.start();
+						}).start();
+					}else {
+						new Worker(ja).start();
+						ja = new JSONArray();
 					}
+					
 					System.out.println(Thread.currentThread().getName() + ": Frames captured: " + n);
 				}
 			}
@@ -161,4 +158,23 @@ public class FacialClient extends JFrame {
 		}
 		return base64Image;
 	}
+	
+	public static String encoder(String imagePath) {
+		String base64Image = "";
+		File file = new File(imagePath);
+		try {
+			FileInputStream imageInFile = new FileInputStream(file);
+			// Reading a Image file from file system
+			byte imageData[] = new byte[(int) file.length()];
+			imageInFile.read(imageData);
+			base64Image = Base64.getEncoder().encodeToString(imageData);
+		} catch (FileNotFoundException e) {
+			System.out.println("Image not found" + e);
+		} catch (IOException ioe) {
+			System.out.println("Exception while reading the Image " + ioe);
+		}
+		return base64Image;
+	}
 }
+
+
---
+++ src/main/java/com/surirobot/Worker.java
@@ -0,0 +1,77 @@
+package com.surirobot;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Worker extends Thread {
+	
+	private JSONArray ja;
+	private JSONObject js;
+	
+	public Worker(JSONArray ja, JSONObject js) {
+		this.ja = ja;
+		this.js = js;
+	}
+	
+	public Worker(JSONArray ja) {
+		this.ja = ja;
+	}
+	
+	@Override
+	public void run() {
+		System.out.println("TAILLE === "+ja.length());
+		//On construit la requete
+		HttpClient client = new DefaultHttpClient();  
+		HttpPost post = new HttpPost(FacialClient.URL_API);
+		HttpResponse response;
+        StringEntity se;
+		try {
+			JSONObject json  = new JSONObject();
+			json.put("pictures", ja);
+			if(js != null)
+				json.put("record", js.getString("record"));
+			se = new StringEntity(json.toString());
+	        se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+	        post.setEntity(se);
+	        response = client.execute(post);
+	        BufferedReader bis = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+	        StringBuilder s = new StringBuilder();
+	        String tmp = "";
+	        
+	        //On lis le résultat
+	        while((tmp = bis.readLine())!=null) {
+	        	s.append(tmp);
+	        }
+	        System.out.println("EMOTION ==== "+s.toString());
+	        
+	        //On affiche le résultat à l'écran
+	        JSONObject res = new JSONObject(s.toString());
+	        StringBuilder str = new StringBuilder();
+	        str.append("Facial : "+res.getJSONArray("facial").toString()+"\n");
+	        try {
+	        	str.append("Vocal : "+res.getJSONObject("vocal").toString()+"\n");
+	        }catch(JSONException e) {}
+	        FacialClient.result.setText(str.toString());
+	        
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+}
